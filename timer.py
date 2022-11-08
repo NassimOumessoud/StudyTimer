93b868e069911e2e6c882f033364ee3c862ba513
Nassim <nassim.oumes@gmail.com>
2022-11-08 11:10:54 +0100

    Trying out threading, since countdown stalls mainloop

--- timer.py
+++ timer.py
@@ -1,9 +1,12 @@
+from argparse import Action
+from concurrent.futures import thread
 from ipaddress import collapse_addresses
 import tkinter as tk
 from tkinter import ttk
 from tkinter.font import Font
 
 import time
+import threading
 import random
 import cProfile
 
@@ -11,7 +14,7 @@ import cProfile
 class StudyTimer():
     def __init__(self, main):
         self.main = main
-
+        
         background_color = '#5dade2'
         menu_color = '#DAF7A6'
         self.title_text = Font(family='Times New Roman', size=20, weight='bold')
@@ -21,7 +24,6 @@ class StudyTimer():
         break_default = 5
         interval_default = '4'
         
-
         #Styles
         self.main.config(bg=background_color)
 
@@ -34,7 +36,7 @@ class StudyTimer():
 
         spinboxstyle = ttk.Style()
         spinboxstyle.theme_use('default')
-        spinboxstyle.configure('My.TSpinbox', arrowsize=20)
+        spinboxstyle.configure('My.TSpinbox', arrowsize=20) 
         
         #Widget
         self.welcome = ttk.Label(self.main, text="Welcome to your study timer!", style="TLabel")
@@ -60,16 +62,30 @@ class StudyTimer():
         self.intervals.insert(0, interval_default)
         self.intervals.grid(column=1, row=3, sticky="W")
 
-        ttk.Button(self.setupFrame, text="Start", command=self.timer).grid(column=0, row=5)
+        print('done')
+        ttk.Button(self.setupFrame, text="Start", command=self.init_countdown()).grid(column=0, row=5)
         ttk.Button(self.setupFrame, text="Exit", command=main.destroy).grid(column=1, row=5)
         
+       
+    def countdown(self, value, intro_line, study=True):
+        secs = 59
+        
+        while value >= 0:
+            self.label['text'] = f"{value} : {secs}"      
 
-    def timer(self):
+            time.sleep(1)
+            secs -= 1
+            if secs == 0:
+                value -= 1
+                secs = 59
+
+
+    def init_countdown(self):
         self.setupFrame.grid_forget()
-        #self.welcome.grid_forget()
+        self.welcome.grid_forget()
 
         self.countdownFrame = ttk.Frame(self.main)
-        self.countdownFrame.grid(column=0, row=1)
+        self.countdownFrame.grid(column=1, row=1)
 
         self.study_label = ttk.Label(self.countdownFrame, text="Study time left -")
         self.break_label = ttk.Label(self.countdownFrame, text="Break time left -")
@@ -82,21 +98,26 @@ class StudyTimer():
                         "Nice job, you deserve a break!",
                         "It's time for a break"]
 
+        
+        study = int(self.high.get()) - 1
+        pause = int(self.low.get()) - 1
+        study_line = random.choice(study_lines)
+        break_line = random.choice(break_lines)
+            
         for interval in range(intervals):
             interval += 1
-            study = int(self.high.get()) - 1
-            pause = int(self.low.get()) - 1
-            study_line = random.choice(study_lines)
-            break_line = random.choice(break_lines)
-            
+
             self.study_label.grid(column=0, row=0)
-            self.countdown(study, study_line)
             self.study_label.grid_forget()
+            self.label = ttk.Label(self.countdownFrame, text=f"{study} : 59", style='TLabel')
+            self.label.grid(column=2, row=0) 
+            count = threading.Thread(target=self.countdown, args=(study, study_line))
+            count.start()
+            
 
             self.break_label.grid(column=0, row=0)
             self.countdown(pause, break_line, study=False)
             self.break_label.grid_forget()
-
 
             if interval == intervals:
                 study = int(self.high.get())
@@ -106,29 +127,6 @@ class StudyTimer():
                 self.welcome.grid()
                 self.setupFrame.grid()
                 break
-                
-
-
-    def countdown(self, value, intro_line, study=True):
-        secs = 59
-        #intro_label = ttk.Label(self.countdownFrame, text=intro_line)
-        #intro_label.grid(column=0, row=0)
-        #time.sleep(10)
-        #print(intro_line)
-        #intro_label.destroy()
-
-        
-        while value >= 0:
-            self.label = ttk.Label(self.countdownFrame, text=f"{value} : {secs}", style='TLabel')
-            self.label.grid(column=2, row=0) 
-            self.label.destroy()
-
-            time.sleep(1)
-            secs -= 1
-            if secs == 0:
-                value -= 1
-                secs = 59
-
         
 
 
